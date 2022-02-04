a23beecdf3f328247013063c57439b7d3797ce27
PaulArhireAccenture <94470261+PaulArhireAccenture@users.noreply.github.com>
2022-02-04 12:38:26 +0200

    Add files via upload

--- Paralel/Program.cs
+++ Paralel/Program.cs
@@ -1,81 +1,50 @@
 ﻿using System;
 using System.Linq;
 using System.Collections;
+using System.Collections.Concurrent;
 using System.Threading;
 using System.Threading.Tasks;
 
 namespace Paralel
 {
-    class TimestampDTO
-    {
-
-        public DateTime timestamp;
-        public int elementID;
-        public int senderID;
-        public int[] list = new int[10];
     
-        public TimestampDTO(int senderID)
-        {
-            this.senderID = senderID;
-            this.timestamp = DateTime.Now;
-            this.elementID = new Random().Next(1000,9999);
-            for(int i = 0; i < 10; i++)
-            {
-                this.list[i] = new Random().Next(1,100);
-            }
-        }
-    }
     class Program
     {
-        static Queue queue = new Queue();
-        static bool canAdd = true;
+        static ConcurrentQueue<TimestampDTO> queue = new ConcurrentQueue<TimestampDTO>();
+        //static bool canAdd = true;
         static void Main(string[] args)
         {
             Task taskA = Task.Run(() => {
-                while (!canAdd) { }
-                canAdd = false;
+                //while (!canAdd) { }
+                //canAdd = false;
                 queue.Enqueue(new TimestampDTO(1));
-                canAdd = true;
+                //canAdd = true;
             });
+            taskA.Wait();
             Task taskB = Task.Run(() => {
-                while (!canAdd) { }
-                canAdd = false;
+                //while (!canAdd) { }
+                //canAdd = false;
                 queue.Enqueue(new TimestampDTO(2));
-                canAdd = true;
+                //canAdd = true;
             });
             Task taskC = Task.Run(() => {
                 ArrayList list = new ArrayList();
-                Console.WriteLine(">>>>>>>>>>>>>>>>>>>>>>>>>");
-                while (!canAdd) { }
+                //while (!canAdd) { }
                 while (queue.Count != 0)
                 {
-                    TimestampDTO aux = (TimestampDTO)queue.Dequeue();
-                    foreach(int i in aux.list)
-                    {
-                        list.Add(i);
-                    }
+                    queue.TryDequeue(out TimestampDTO aux);
+                    list = aux.list;
                     list.Sort();
-                    double avg = 0;
-                    int count = 0;
-                    foreach(int i in list)
-                    {
-                        avg += i;
-                    }
-                    avg = avg / list.Count;
-                    foreach(int i in list)
-                    {
-                        if(i > avg)
-                        {
-                            count++;
-                        }
-                    }
+                    double avg = (from int number in list select number).Average();
+                    int count = (from int number in list where number > avg select number).Count();
+                    
                     DateTime currTime = DateTime.Now;
                     
                     Console.WriteLine("The DTO with senderID: " + aux.senderID + " has " + count + " elements over " + avg);
                     Console.WriteLine("The process took: " + (currTime - aux.timestamp) + " seconds");
                 }
             });
-            taskA.Wait();
+            //taskA.Wait();
             taskB.Wait();
             taskC.Wait();
 
---
+++ Paralel/TimestampDTO.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Paralel
+{
+    class TimestampDTO
+    {
+        public DateTime timestamp;
+        public int elementID;
+        public int senderID;
+        public ArrayList list = new ArrayList();
+
+        public TimestampDTO(int senderID)
+        {
+            this.senderID = senderID;
+            this.timestamp = DateTime.Now;
+            this.elementID = new Random().Next(1000, 9999);
+            for (int i = 0; i < 10; i++)
+            {
+                this.list.Add(new Random().Next(1, 100));
+            }
+        }
+    }
+}
